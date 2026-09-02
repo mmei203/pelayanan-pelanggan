d8fdf4f2573aa2820d0a46686f4aef023b7683d7
mmei203 <meisyandaryani@gmail.com>
2026-09-02 19:51:36 +0800

    tambah middlewares

--- server.js
+++ server.js
@@ -4,14 +4,31 @@ import pelangganRoute from "./src/routes/pelanggan.routes.js";
 import historyRoute from "./src/routes/history.routes.js";
 import authRoute from './src/routes/auth.routes.js';
 import errorMiddleware from "./src/middlewares/error.middleware.js";
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import {rateLimit} from 'express-rate-limit';
 
 const server = express();
 const PORT = process.env.PORT;
 
+// MIDDLEWARES
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(errorMiddleware);
+server.use(cors());
+server.use(morgan('dev'));
+server.use(helmet());
 
+const limiter = rateLimit({
+   windowMs: 5 * 60 * 1000, //timeout sampe kapan
+   limit: 20,
+   message: "too much request"
+})
+server.use(limiter);
+
+
+// ROUTES
 server.use('/pelanggan', pelangganRoute);
 server.use('/history', historyRoute);
 server.use('/auth', authRoute);
