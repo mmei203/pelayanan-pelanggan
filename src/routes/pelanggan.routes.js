ef00cd1f6baa070c1c22efd3b98a163498f00621
mmei203 <meisyandaryani@gmail.com>
2026-09-03 18:59:35 +0800

    tambah route get pelanggan by status

--- src/routes/pelanggan.routes.js
+++ src/routes/pelanggan.routes.js
@@ -2,6 +2,7 @@ import {Router} from 'express';
 import { 
    detailPelanggan, 
    getPelanggan, 
+   pelangganByStatus, 
    postPelanggan, 
    updatePelanggan 
 } from '../controllers/pelanggan.controller.js';
@@ -13,6 +14,9 @@ pelangganRoute.get('/',getPelanggan);
 
 // GET PELANGGAN BY NAME
 pelangganRoute.get('/:id',detailPelanggan);
+
+// GET PELANGGAN BY STATUS
+pelangganRoute.get('/:status', pelangganByStatus);
 
 // ADD PELANGGAN
 pelangganRoute.post('/tambah-pelanggan', postPelanggan);
